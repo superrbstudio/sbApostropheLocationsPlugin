af349201aa5c4a474d02c5f9152e29e80fbf5286
Giles Smith <giles@superrb.com>
2011-12-02 12:19:28 +0000

    Created location lookup object that caches results from API

--- lib/actions/BasesbLocationsLookupActions.class.php
+++ lib/actions/BasesbLocationsLookupActions.class.php
@@ -6,43 +6,32 @@ abstract class BasesbLocationsLookupActions extends BaseaActions
 	{
 		$this->forward404Unless($this->getUser()->isAuthenticated());
 		$this->getResponse()->setHttpHeader('Content-Type','application/json; charset=utf-8');
+		
 		switch($request->getParameter('lookup'))
 		{
 			case 'address':
-				$success = $this->lookupAddress($request->getParameter('address'), sfConfig::get('app_sbLocations_google_geocode_sensor_usage', false));
-				if(is_array($success)){ $data = $success; $success = true; }
+				$lookup = new sbLookupAddress(array('address' => $request->getParameter('address'),
+																						'api_url' => sfConfig::get('app_sbLocations_google_geocode_lookup_url')));
+				
+				if($lookup->lookupGeolocationFromAddress())
+				{
+					$success = 'OK';
+					$data = array('address' => $lookup->getAddress(),
+												'latitude' => $lookup->getLatitude(),
+												'longitude' => $lookup->getLongitude(),
+												'real_address' => $lookup->getRealAddress(),
+												'formatted_address' => $lookup->getFormattedAddress(),
+												'from_cache' => $lookup->getDataPulledFromCache());
+				}
 				break;
 			
 			default:
-				$success = false;
+				$success = 'REQUEST_DENIED';
 				$data = array();
 				break;
 		}
 		
-		if($success)
-		{
-			$data = json_encode(array('results' => $data, 'status' => 'OK'));
-		}
-		else
-		{
-			$data = json_encode(array('results' => array(), 'status' => 'REQUEST_DENIED'));
-		}
-		
-		$this->getResponse()->setContent($data);
+		$this->getResponse()->setContent(json_encode(array('results' => $data, 'status' => $success)));
 		return sfView::NONE;
-	}
-	
-	protected function lookupAddress($address, $sensor = false)
-	{
-		if($sensor == true) { $sensor = 'true'; } else { $sensor = 'false'; }
-		$params = http_build_query(array('address' => $address, 'sensor' => $sensor));
-		$result = json_decode(file_get_contents(sfConfig::get('app_sbLocations_google_geocode_lookup_url') . '?' . $params));
-
-		if($result and $result->status == 'OK')
-		{
-			return $result->results;
-		}
-		
-		return false;
 	}
 }
