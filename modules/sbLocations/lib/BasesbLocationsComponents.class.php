8b6fc36e63e17609aad437ea2151a0d16c1277aa
Giles Smith <giles@superrb.com>
2011-12-14 19:49:19 +0000

    Put in check to make sure params are an array

--- modules/sbLocations/lib/BasesbLocationsComponents.class.php
+++ modules/sbLocations/lib/BasesbLocationsComponents.class.php
@@ -14,6 +14,7 @@ abstract class BasesbLocationsComponents extends sfComponents
 		$defaults = array('class' => 'sb-location-main',
 											'dimensions' => sfConfig::get('app_sbLocations_slideshow', array('width' => 600, 'height' => 400)));
 		$this->sbLocation = $this->location;
+		if(!is_array($this->params)){ $this->params = array(); }
 		$this->params     = array_merge($defaults, $this->params);
 	}
 }
