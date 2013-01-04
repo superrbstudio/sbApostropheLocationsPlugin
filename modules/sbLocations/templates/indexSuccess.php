a54299740680bd95934c0bf47e01894e09dd233e
Giles Smith <giles@superrb.com>
2013-01-04 14:59:38 +0000

    Basic index page

--- modules/sbLocations/templates/indexSuccess.php
+++ modules/sbLocations/templates/indexSuccess.php
@@ -1,5 +1,15 @@
-<?php
+<?php $pagerUrl = url_for('sbLocations/index'); ?>
 
-/** output all locations here **/
+<?php if ($pager->haveToPaginate()): ?>
+  	 <?php include_partial('aPager/pager', array('pager' => $pager, 'pagerUrl' => $pagerUrl)) ?>
+  <?php endif ?>
 
-?>
+<?php foreach($pager->getResults() as $sbLocation): ?>
+<div class="sb-location">
+  <a href="<?php echo url_for('@sb_location?slug=' . $sbLocation['slug']); ?>"><?php echo $sbLocation['title']; ?></a>
+</div>
+<?php endforeach; ?>
+
+<?php if ($pager->haveToPaginate()): ?>
+  	 <?php include_partial('aPager/pager', array('pager' => $pager, 'pagerUrl' => $pagerUrl)) ?>
+  <?php endif ?>
