ab1ea0ea93e7b06cebfc054d7144d7b4a02e7b7e
Giles Smith <giles@superrb.com>
2013-01-08 11:04:25 +0000

    Display a map title if passed

--- modules/sbLocations/templates/_DisplayMap.php
+++ modules/sbLocations/templates/_DisplayMap.php
@@ -1,2 +1,5 @@
 <div id="sb-locations-map-container"></div>
+<?php if(isset($mapTitle)): ?>
+<h2 class="sb-location-map-title"><?php echo html_entity_decode($mapTitle); ?></h2>
+<?php endif; ?>
 <?php a_js_call('sbLocationsLoadMap(?)', html_entity_decode($markersUrl)); ?>
