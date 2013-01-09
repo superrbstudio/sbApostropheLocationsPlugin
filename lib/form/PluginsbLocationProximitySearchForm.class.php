e29a3cfbeeacc66c79dfcf09effe58c18bcb06c7
Giles Smith <giles@superrb.com>
2013-01-09 15:35:38 +0000

    Added proximity search form for location listin pages

---
+++ lib/form/PluginsbLocationProximitySearchForm.class.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Description of PluginsbLocationProximitySearchForm
+ *
+ * @author giles
+ */
+class PluginsbLocationProximitySearchForm extends sfForm 
+{
+  protected $distanceChoices;
+  
+  public function setup()
+  {
+    parent::setup();
+    
+    $this->disableCSRFProtection();
+    
+    $this->setWidget('search', new sfWidgetFormInputText(array('label' => 'Your Location (Postcode)')));
+    $this->setValidator('search', new sfValidatorString(array('required' => true), array('required' => 'Please enter a Search Location')));
+    
+    
+    $this->setWidget('distance', new sfWidgetFormChoice(array('choices' => $this->getDistanceChoices())));
+    $this->setValidator('distance', new sfValidatorChoice(array('choices' => array_keys($this->getDistanceChoices()))));
+    
+    $this->widgetSchema->setNameFormat('proximity[%s]');
+  }
+  
+  protected function getDistanceChoices()
+  {
+    if(!isset($this->distanceChoices))
+    {
+      $unit = sbLocationTable::getUnit();
+      $this->distanceChoices = array(
+          1 => '1' . $unit->abbr, 
+          5 => '5' . $unit->abbr . 's', 
+          10 => '10' . $unit->abbr . 's', 
+          25 => '25' . $unit->abbr . 's',
+          50 => '50' . $unit->abbr . 's'
+      );
+    }
+    
+    return $this->distanceChoices;
+  }
+}
